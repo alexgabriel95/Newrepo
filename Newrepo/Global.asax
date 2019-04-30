55c9bba6c1070bf856d42e7509ed39677cf95f1a
Alex Gabriel <Rios Velazquez>
2019-04-30 16:23:01 -0300

    ASP.NET MVC Fundamentals

---
+++ Newrepo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Newrepo.MvcApplication" Language="C#" %>
---
+++ Newrepo/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newrepo.Models;
+
+namespace Newrepo.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
