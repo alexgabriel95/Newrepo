e32021ce6769a5844af4cc7162fde0b7e8e93fcd
Alex Gabriel Rios Velazquez <alex.grvelazquez@hotmail.com>
2019-05-06 16:11:33 -0300

    fin section 1

---
+++ Newrepo/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newrepo.Models;
+
+namespace Newrepo.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomer();
+
+            return View(customers);
+        }
+
+
+        public ActionResult Detail(int id)
+        {
+            var customer = GetCustomer().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null) 
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+
+    }
+}
--- Newrepo/Controllers/MoviesController.cs
+++ Newrepo/Controllers/MoviesController.cs
@@ -11,6 +11,21 @@ namespace Newrepo.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek!" },
+                new Movie { Id = 2, Name = "Titanic" }
+            };
+        }
+
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
@@ -28,57 +43,57 @@ namespace Newrepo.Controllers
 
             return View(viewModel);
         }
-        [Route("movies/released/{year}/{month:regex(\\d{2}:range{1, 12})}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
 
 
 
-            return Content(year + "/" + month);
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //public ActionResult Edit(int id)
+        //public ActionResult Random()
         //{
-            
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1" },
+        //        new Customer { Name = "Customer 2" }
+        //    };
 
-        //    return Content("id=" +id);
-        //}
-        
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-        //    if (String.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
-        //    return Content(String.Format("pageIngex={0}&sortBy={1}", pageIndex, sortBy));
-        //}
+    //    var viewModel = new RandomMovieViewModel
+    //    {
+    //        Movie = movie,
+    //        Customers = customers
+    //    };
 
-    }
+    //        return View(viewModel);
+    //}
+
+
+    //public ActionResult Edit(int id)
+    //{
+
+
+    //    return Content("id=" +id);
+    //}
+
+    //public ActionResult Index(int? pageIndex, string sortBy)
+    //{
+    //    if (!pageIndex.HasValue)
+    //        pageIndex = 1;
+    //    if (String.IsNullOrWhiteSpace(sortBy))
+    //        sortBy = "Name";
+    //    return Content(String.Format("pageIngex={0}&sortBy={1}", pageIndex, sortBy));
+    //}
+
+
+
+
+
+    //[Route("movies/released/{year}/{month:regex(\\d{2}:range{1, 12})}")]
+
+
+    //public ActionResult ByReleaseDate(int year, int month)
+    //{
+
+
+
+    //    return Content(year + "/" + month);
+    //}
+}
 }
