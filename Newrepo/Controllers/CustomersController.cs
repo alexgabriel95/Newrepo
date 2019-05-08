7010b48df641b45e29365df0dc983fc565c5a403
Alex Gabriel Rios Velazquez <alex.grvelazquez@hotmail.com>
2019-05-08 11:44:20 -0300

    Exercise2

--- Newrepo/Controllers/CustomersController.cs
+++ Newrepo/Controllers/CustomersController.cs
@@ -32,7 +32,7 @@ namespace Newrepo.Controllers
 
         public ActionResult Detail(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null) 
             {
